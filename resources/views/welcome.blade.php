c77a00148b8b21f5efa2670583b78b934b14c144
unknown <informaciones.ite@gmail.com>
2024-06-07 05:38:17 -0400

    corrigiendo storage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -172,7 +172,7 @@
                         <div class="col-6 col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-0"> 
                             <a href="{{ url("show/producto") }}/${product.id}" class="product-link">
                                 <div class="product-card">
-                                    <img src="{{ storage_path('app/public/products/'}}/${product.product_image}" alt="Producto ${product.id}">
+                                    <img src="{{ storage_path('app/public/products/')}}/${product.product_image}" alt="Producto ${product.id}">
                                     <div class="card-body">
                                         <p class="price mb-0">Bs. ${product.precio1} <span>${product.product_name}</span></p> 
                                         <h5 class="description">${product.descripcion}</h5>
